937dbda4568b77f60d2a4342318cd9abebfa8a9d
q <q@qs-Mac.local>
2015-06-11 20:37:41 +0200

    layout changed: room added for three labels toolbar missing buttons

--- MapKitDemo/ViewController.swift
+++ MapKitDemo/ViewController.swift
@@ -6,12 +6,16 @@
 //  Copyright (c) 2015 Prikic. All rights reserved.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mMapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +24,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func changeMapMode(sender: UIBarButtonItem) {
+        
+        let buttonTitle = sender.title!
+        
+        switch buttonTitle {
+        
+        case "Hybrid":
+            
+            mMapView.mapType = .Hybrid
+        
+        case "Satellite":
+        
+            mMapView.mapType = .Satellite
+        
+        case "Standard":
+            
+            mMapView.mapType = .Standard
+            
+        default:
+            
+            break
+                        
+        }
+        
+            
+    }
+    
+        
 
 }
 
